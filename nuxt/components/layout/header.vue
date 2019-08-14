aa3cca27d617402effd75904ad83347ed4a45d1e
Sil van Diepen <me@silvandiepen.com>
2019-08-14 09:30:36 +0200

    Create header.vue

---
+++ nuxt/components/layout/header.vue
@@ -0,0 +1,35 @@
+<template>
+	<header id="header" class="header">
+		<h3>{{ projectName }}</h3>
+		<LayoutNavigation></LayoutNavigation>
+	</header>
+</template>
+
+<script>
+import project from '~/package.json';
+import LayoutNavigation from '~/components/layout/navigation.vue';
+export default {
+	components: {
+		LayoutNavigation
+	},
+	data() {
+		return {
+			projectName: project.name
+		};
+	}
+};
+</script>
+
+<style lang="scss">
+@import '~tools';
+
+.header {
+	// Header styles
+	position: fixed;
+	top: 0;
+	left: 0;
+
+	width: 100%;
+	text-align: center;
+}
+</style>
