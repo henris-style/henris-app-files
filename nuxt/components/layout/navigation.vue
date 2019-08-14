2add1b260f1c131c5c9d863965d7ee0f5fdd3cde
Sil van Diepen <me@silvandiepen.com>
2019-08-14 09:30:22 +0200

    Create navigation.vue

---
+++ nuxt/components/layout/navigation.vue
@@ -0,0 +1,30 @@
+<template>
+	<nav class="navigation">
+		<ul class="navigation__list">
+			<li class="navigation__item" v-for="(item, idx) in navigation" :key="idx">
+				<nuxt-link class="navigation__link" :to="item.url">
+					<span class="navigation__text">
+						{{ item.title }}
+					</span>
+				</nuxt-link>
+			</li>
+		</ul>
+	</nav>
+</template>
+
+<script>
+export default {
+	data: () => {
+		navigation: [
+			{
+				title: 'Home',
+				url: '/'
+			}
+		];
+	}
+};
+</script>
+
+<style lang="scss">
+@import '~tools';
+</style>
