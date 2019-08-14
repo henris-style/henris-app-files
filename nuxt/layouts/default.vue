6a32f5b95a6ab958fa479cb24debd68f2342bbbd
Sil van Diepen <me@silvandiepen.com>
2019-08-14 09:32:46 +0200

    Create default.vue

---
+++ nuxt/layouts/default.vue
@@ -0,0 +1,19 @@
+<template>
+	<div class="layout layout-default">
+		<LayoutHeader />
+		<nuxt />
+		<LayoutFooter />
+	</div>
+</template>
+
+<script>
+import LayoutHeader from '~/components/layout/header.vue';
+import LayoutFooter from '~/components/layout/footer.vue';
+
+export default {
+	components: {
+		LayoutHeader,
+		LayoutFooter
+	}
+};
+</script>
