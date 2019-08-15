b843df9d580df4e9d89f48e27a323365d569bd40
Sil van Diepen <me@silvandiepen.com>
2019-08-15 11:19:33 +0200

    Create index.vue

---
+++ nuxt/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <main class="page">
+    <HenrisIntro></HenrisIntro>
+  </main>
+</template>
+
+<script>
+import HenrisIntro from '~/components/intro.vue';
+
+export default{
+  components: { HenrisIntro }
+}
+</script>
+
+<style lang="scss">
+@import '~tools';
+</style>
