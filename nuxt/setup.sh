4c4266565168e989bf5e2d6bfd13750cf7d847fa
Sil van Diepen <me@silvandiepen.com>
2019-08-15 11:12:16 +0200

    keep silent on success, but show errors on fail

--- nuxt/setup.sh
+++ nuxt/setup.sh
@@ -8,18 +8,18 @@ printf '\n  \e[1;34mSetup Nuxt Files\e[0m'
 # Assets
 mkdir -p ${TARGET}assets/fonts
 
-curl -o ${TARGET}assets/fonts/README.md -l ${URL}/assets/fonts/README.md --silent
+curl -o ${TARGET}assets/fonts/README.md -l ${URL}/assets/fonts/README.md --fail --silent --show-error
 
 printf '\n  Assets \e[1;34mcreated!\e[0m' 
 
 # Components
 mkdir -p ${TARGET}components/layout
 
-curl -o ${TARGET}components/intro.vue -l ${URL}/components/intro.vue --silent
-curl -o ${TARGET}components/layout/header.vue -l ${URL}/components/layout/header.vue --silent
-curl -o ${TARGET}components/layout/footer.vue -l ${URL}/components/layout/footer.vue --silent
-curl -o ${TARGET}components/layout/navigation.vue -l ${URL}/components/layout/navigation.vue --silent
-curl -o ${TARGET}components/README.md -l ${URL}/components/README.md --silent
+curl -o ${TARGET}components/intro.vue -l ${URL}/components/intro.vue --fail --silent --show-error
+curl -o ${TARGET}components/layout/header.vue -l ${URL}/components/layout/header.vue --fail --silent --show-error
+curl -o ${TARGET}components/layout/footer.vue -l ${URL}/components/layout/footer.vue --fail --silent --show-error
+curl -o ${TARGET}components/layout/navigation.vue -l ${URL}/components/layout/navigation.vue --fail --silent --show-error
+curl -o ${TARGET}components/README.md -l ${URL}/components/README.md --fail --silent --show-error
 
 printf '\n  Components \e[1;34mcreated!\e[0m' 
 
@@ -27,26 +27,26 @@ printf '\n  Components \e[1;34mcreated!\e[0m'
 # Config
 mkdir ${TARGET}config 
 
-curl -o ${TARGET}config/dev.sh -l ${URL}/config/dev.sh --silent
-curl -o ${TARGET}config/local.sh -l ${URL}/config/local.sh --silent
-curl -o ${TARGET}config/production.sh -l ${URL}/config/production.sh --silent
-curl -o ${TARGET}config/staging.sh -l ${URL}/config/staging.sh --silent
-curl -o ${TARGET}config/README.md -l ${URL}/config/README.md --silent
+curl -o ${TARGET}config/dev.sh -l ${URL}/config/dev.sh --fail --silent --show-error
+curl -o ${TARGET}config/local.sh -l ${URL}/config/local.sh --fail --silent --show-error
+curl -o ${TARGET}config/production.sh -l ${URL}/config/production.sh --fail --silent --show-error
+curl -o ${TARGET}config/staging.sh -l ${URL}/config/staging.sh --fail --silent --show-error
+curl -o ${TARGET}config/README.md -l ${URL}/config/README.md --fail --silent --show-error
 
 printf '\n  Config \e[1;34mcreated!\e[0m' 
 
 # Layouts
 mkdir ${TARGET}layouts 
 
-curl -o ${TARGET}layouts/index.vue -l ${URL}/layouts/index.vue --silent
-curl -o ${TARGET}layouts/README.md -l ${URL}/layouts/README.md --silent
+curl -o ${TARGET}layouts/index.vue -l ${URL}/layouts/index.vue --fail --silent --show-error
+curl -o ${TARGET}layouts/README.md -l ${URL}/layouts/README.md --fail --silent --show-error
 
 printf '\n  Layouts \e[1;34mcreated!\e[0m' 
 
 # Middleware
 mkdir ${TARGET}middleware 
 
-curl -o ${TARGET}middleware/README.md -l ${URL}/middleware/README.md --silent
+curl -o ${TARGET}middleware/README.md -l ${URL}/middleware/README.md --fail --silent --show-error
 
 printf '\n  Middleware \e[1;34mcreated!\e[0m' 
 
@@ -54,19 +54,19 @@ printf '\n  Middleware \e[1;34mcreated!\e[0m'
 # Pages
 mkdir ${TARGET}pages 
 
-curl -o pages/index.vue -l ${URL}/pages/index.vue --silent
-curl -o pages/README.md -l ${URL}/pages/README.md --silent
+curl -o pages/index.vue -l ${URL}/pages/index.vue --fail --silent --show-error
+curl -o pages/README.md -l ${URL}/pages/README.md --fail --silent --show-error
 
 printf '\n  Pages \e[1;34mcreated!\e[0m' 
 
 # Scripts
 mkdir ${TARGET}scripts 
 
-curl -o scripts/dev.sh -l ${URL}/scripts/dev.sh --silent
-curl -o scripts/local.sh -l ${URL}/scripts/local.sh --silent
-curl -o scripts/production.sh -l ${URL}/scripts/production.sh --silent
-curl -o scripts/staging.sh -l ${URL}/scripts/staging.sh --silent
-curl -o scripts/README.md -l ${URL}/scripts/README.md --silent
+curl -o scripts/dev.sh -l ${URL}/scripts/dev.sh --fail --silent --show-error
+curl -o scripts/local.sh -l ${URL}/scripts/local.sh --fail --silent --show-error
+curl -o scripts/production.sh -l ${URL}/scripts/production.sh --fail --silent --show-error
+curl -o scripts/staging.sh -l ${URL}/scripts/staging.sh --fail --silent --show-error
+curl -o scripts/README.md -l ${URL}/scripts/README.md --fail --silent --show-error
 
 printf '\n  Scripts \e[1;34mcreated!\e[0m' 
 
@@ -85,7 +85,7 @@ printf '\n  Static \e[1;34mcreated!\e[0m'
 # Static
 mkdir ${TARGET}plugins 
 
-curl -o ${TARGET}plugins/README.md -l ${URL}/plugins/README.md --silent
+curl -o ${TARGET}plugins/README.md -l ${URL}/plugins/README.md --fail --silent --show-error
 
 printf '\n  Plugins \e[1;34mcreated!\e[0m' 
 
@@ -93,10 +93,10 @@ printf '\n  Plugins \e[1;34mcreated!\e[0m'
 # Static
 mkdir -p ${TARGET}static/images
 
-curl -o ${TARGET}static/README.md -l ${URL}/static/README.md --silent
-curl -o ${TARGET}static/favicon.ico -l ${URL}/static/favicon.ico --silent
-curl -o ${TARGET}static/icon.png -l ${URL}/static/icon.png --silent
-curl -o ${TARGET}static/images/README.md -l ${URL}/static/images/README.md --silent
+curl -o ${TARGET}static/README.md -l ${URL}/static/README.md --fail --silent --show-error
+curl -o ${TARGET}static/favicon.ico -l ${URL}/static/favicon.ico --fail --silent --show-error
+curl -o ${TARGET}static/icon.png -l ${URL}/static/icon.png --fail --silent --show-error
+curl -o ${TARGET}static/images/README.md -l ${URL}/static/images/README.md --fail --silent --show-error
 
 printf '\n  Static \e[1;34mcreated!\e[0m' 
 
@@ -104,16 +104,16 @@ printf '\n  Static \e[1;34mcreated!\e[0m'
 # Store
 mkdir ${TARGET}store 
 
-curl -o ${TARGET}store/README.md -l ${URL}/store/README.md --silent
+curl -o ${TARGET}store/README.md -l ${URL}/store/README.md --fail --silent --show-error
 
 printf '\n  Store \e[1;34mcreated!\e[0m' 
 
 # Linters
-curl -o ${TARGET}.babelrc -l ${URL}/.babelrc --silent
-curl -o ${TARGET}.eslintrc.js -l ${URL}/.eslintrc.js --silent
-curl -o ${TARGET}.gitignore -l ${URL}/.gitignore --silent
-curl -o ${TARGET}.prettierrc -l ${URL}/.prettierrc --silent
-curl -o ${TARGET}nuxt.config.js -l ${URL}/nuxt.config.js --silent
-curl -o ${TARGET}README.md -l ${URL}/README.md --silent
+curl -o ${TARGET}.babelrc -l ${URL}/.babelrc --fail --silent --show-error
+curl -o ${TARGET}.eslintrc.js -l ${URL}/.eslintrc.js --fail --silent --show-error
+curl -o ${TARGET}.gitignore -l ${URL}/.gitignore --fail --silent --show-error
+curl -o ${TARGET}.prettierrc -l ${URL}/.prettierrc --fail --silent --show-error
+curl -o ${TARGET}nuxt.config.js -l ${URL}/nuxt.config.js --fail --silent --show-error
+curl -o ${TARGET}README.md -l ${URL}/README.md --fail --silent --show-error
 
 printf '\n  Config files \e[1;34mcreated!\e[0m\n\n' 
