9d0c3a27a61b2e6837fa236ea8996efd8736c441
Sil van Diepen <me@silvandiepen.com>
2019-08-19 16:04:53 +0200

    Package name update

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -20,7 +20,7 @@ module.exports = {
 	 ** Headers of the page
 	 */
 	head: {
-		title: "henri's",
+		title: pkg.name,
 		meta: [
 			{ charset: 'utf-8' },
 			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
