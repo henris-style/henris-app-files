a9a65e451e75285bc0b96d8eaa45b81b8b57f9c9
Sil van Diepen <me@silvandiepen.com>
2019-09-04 11:23:09 +0200

    Update navigation.vue

--- nuxt/components/layout/navigation.vue
+++ nuxt/components/layout/navigation.vue
@@ -2,11 +2,20 @@
 	<nav class="navigation">
 		<ul class="navigation__list">
 			<li v-for="(item, idx) in nav" :key="idx" class="navigation__item">
-				<nuxt-link class="navigation__link" :to="item.path">
+				<NuxtLink class="navigation__link" :to="item.path" :aria-label="`Go to ${subitem.name}`">
 					<span class="navigation__text">
 						{{ item.name }}
 					</span>
-				</nuxt-link>
+				</NuxtLink>
+				<ul v-if="item.children.length > 0" class="navigation__sublist">
+					<li v-for="(subitem, idy) in item.children" :key="idy" class="navigation__item navigation__subitem">
+						<NuxtLink class="navigation__link navigation__sublink" :to="subitem.path" :aria-label="`Go to ${subitem.name}`">
+							<span class="navigation__text navigation__subtext">
+								{{ subitem.name }}
+							</span>
+						</NuxtLink>
+					</li>
+				</ul>
 			</li>
 		</ul>
 	</nav>
@@ -15,18 +24,26 @@
 <script>
 export default {
 	computed: {
-	nav() {
-		return this.$router.options.routes
-			.filter((route) => route.path.indexOf(':') < 0)
-			.map((route) => {
-				return {
-					name: route.name,
-					path: route.path
-				};
-			});
+		nav() {
+			return this.$router.options.routes
+				.filter((route) => route.path.indexOf(':') < 0)
+				.filter((route) => route.path.indexOf('404') < 0)
+				.filter((route) => route.path.substring(1).indexOf('/') < 0)
+				.filter((route) => route.name !== 'index')
+				.map((route) => {
+					return {
+						name: route.name,
+						path: route.path,
+						children: this.$router.options.routes
+							.filter((route) => route.path.indexOf(':') < 0)
+							.filter((subroute) => subroute.path.indexOf(route.name) > 0 && subroute.path !== route.path)
+							.map((subroute) => {
+								return { name: subroute.name.substr(route.name.length + 1), path: subroute.path };
+							})
+					};
+				});
 		}
 	}
-};
 </script>
 
 <style lang="scss">
