5d874fb8325bc4d9fa2858ab2cdb765b5f6de728
Sil van Diepen <me@silvandiepen.com>
2019-10-30 13:24:43 +0100

    Create about.vue

---
+++ nuxt/pages/about.vue
@@ -0,0 +1,22 @@
+<template>
+    <main class="page page--about">
+        <div class="row center">
+            <div class="column small-full medium-two-third">
+                <div class="content">
+                    <h1>About</h1>
+                </div>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import '~tools';
+.page--about {
+	padding: grid(3) 0;
+}
+</style>
